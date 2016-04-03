1f5cd9cc56c3a3f420e57c6fde3edf0f22fe2f9e
John Rudolph <john.rudolph08@gmail.com>
2016-04-03 00:17:30 -0700

    added simple socket task

--- echo_sockets/socket_tools.py
+++ echo_sockets/socket_tools.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 
 def get_constants(prefix):
@@ -10,12 +11,22 @@ types = get_constants('SOCK_')
 protocols = get_constants('IPPROTO_')
 
 
-def get_address_info(host, port):
-    for response in socket.getaddrinfo(host, port):
-        fam, typ, pro, nam, add = response
-        print('family: {}'.format(families[fam]))
-        print('type: {}'.format(types[typ]))
-        print('protocol: {}'.format(protocols[pro]))
-        print('canonical name: {}'.format(nam))
-        print('socket address: {}'.format(add))
-        print()
+def get_address_info(port):
+	host = 'localhost'
+	for response in socket.getaddrinfo(host, port):
+		fam, typ, pro, nam, add = response
+		print('socket address: {}'.format(add))
+		print('family: {}'.format(families[fam]))
+		print('type: {}'.format(types[typ]))
+		print('protocol: {}'.format(protocols[pro]))
+		print('canonical name: {}'.format(nam))
+
+def get_port_info(lowerbound=1, upperbound=10):
+
+	if 0 <= lowerbound and upperbound <= 65535 and lowerbound <= upperbound:
+		for portnum in range(lowerbound, upperbound):
+			print(get_address_info(portnum))
+	else:
+		raise ValueError('Invalid port range. Valid ports are 0-65535')
+		
+get_port_info(10, 20)
